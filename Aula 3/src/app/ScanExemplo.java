bb2bd4774c6695daf81a610421e397e701a12e51
ALEXANDRE BAGINI <bv3074846@ad.sbv.ifsp.edu.br>
2026-08-20 09:17:20 -0300

    reformulação do repositorio

---
+++ Aula 3/src/app/ScanExemplo.java
@@ -0,0 +1,17 @@
+package app;
+
+import java.util.Scanner;
+
+//Leitura do teclado
+public class ScanExemplo {
+    public static void main(String[] args){
+        //teclado é uma variável do tipo Scanner
+        // teclado é um objeto da classe Scanner
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número inteiro:");
+        //double valor = teclado.nextDouble();
+        //boolean valor= teclado.nextBoolean();
+        int valor = teclado.nextInt();
+        System.out.println("O valor digitado foi "+ valor);
+    }
+}

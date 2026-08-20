6f7ec00a0b43e4fa14a43f6164266de57a3dd849
ALEXANDRE BAGINI <bv3074846@ad.sbv.ifsp.edu.br>
2026-08-20 09:20:48 -0300

    Aula BigInteger Exemplo fatorial

--- Aula 3/src/app/Fatorial.java
+++ Aula 3/src/app/Fatorial.java
@@ -1,4 +1,44 @@
 package app;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
 public class Fatorial {
+
+    public static Integer fatorial(Integer n){
+        Integer fat = 1;
+
+
+        for(Integer i = 1; i <= n; i++){
+            fat = fat * i;
+        }
+
+        return fat;
+    }
+
+    public static BigInteger bigFatorial(Integer n){
+        BigInteger fat = BigInteger.ONE;
+
+        for(Integer i = 1; i <= n; i++){
+            // fat = fat * i;
+            fat = fat.multiply(BigInteger.valueOf(i));
+        }
+
+        return fat;
+
+    }
+
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        Integer n = scan.nextInt();
+        scan.close();
+
+
+        BigInteger resultado = bigFatorial(n);
+        System.out.println("Valor fatorial do numero: " + resultado);
+
+    }
+
 }

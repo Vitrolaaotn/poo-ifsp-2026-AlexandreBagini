628c0874c4c587baebf03b9a758bf6e9aa1a40a4
ALEXANDRE BAGINI <bv3074846@ad.sbv.ifsp.edu.br>
2026-08-20 10:03:37 -0300

    Exercicios HackerRank IfElse & Loops

---
+++ Exercicios/src/app/nextLine.java
@@ -0,0 +1,20 @@
+package app;
+
+    import java.util.Scanner;
+
+    public class nextLine {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int i = scan.nextInt();
+            double d = scan.nextDouble();
+            scan.nextLine(); //Se você utilizar o metodo nextLine() logo após o metodo nextInt(), lembre-se de que o nextInt() lê tokens do tipo inteiro por isso, o caractere de nova linha final daquela linha de entrada de inteiro permanece na fila do buffer de entrada, e a próxima chamada de nextLine() lerá o restante da linha do inteiro (que está vazio).
+
+            String  s = scan.nextLine();
+
+            System.out.println("String: " + s);
+            System.out.println("Double: " + d);
+            System.out.println("Int: " + i);
+        }
+    }
+

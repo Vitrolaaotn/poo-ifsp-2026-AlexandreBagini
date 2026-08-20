24b4f516c3f05f52e389738dd3db2b85c796345b
ALEXANDRE BAGINI <bv3074846@ad.sbv.ifsp.edu.br>
2026-08-20 09:32:03 -0300

    Exercicios Básicos HackerRank

---
+++ Exercicios/src/app/PrintDeTresNumeros.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.*;
+
+public class PrintDeTresNumeros {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b =scan.nextInt();
+        int c = scan.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}

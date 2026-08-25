1f5b07b2feec9ab88163ff195cbf3520f256a069
ALEXANDRE BAGINI <bv3074846@ad.sbv.ifsp.edu.br>
2026-08-25 10:54:44 -0300

    aula de Array e For enhanced

---
+++ Atividades/src/Exercicios/ArrayMatrizes.java
@@ -0,0 +1,27 @@
+package Exercicios;
+
+import java.util.Arrays;
+
+public class ArrayMatrizes {
+    public static void main(String[] args) {
+        // tipo [] nome = new tipo [tamanho]
+        boolean [] sensores = new boolean[5];
+        // 4º posição para true
+        sensores[3] = true;
+        //for (inicio; condição de parada; passo)
+        for(int i=0; i < sensores.length; i++){
+            sensores[i]=true;
+            System.out.println(sensores[i]);
+        }
+        //É possivel converter o tipo do array
+        System.out.println(Arrays.toString(sensores));
+
+        //for enhanced
+        // for (variavel : array)
+        for (boolean sensor :sensores){
+            //System.out.println(sensores [i]);
+
+        }
+    }
+
+}

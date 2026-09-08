d16da33314a21f8fb954ea9a7993dc62db0475d5
ALEXANDRE BAGINI <bv3074846@ad.sbv.ifsp.edu.br>
2026-09-08 12:08:52 -0300

    Adição do metodo ultimoNome

--- Atividades/src/Exercicios/Entidades/Pessoa.java
+++ Atividades/src/Exercicios/Entidades/Pessoa.java
@@ -3,15 +3,20 @@ package Exercicios.Entidades;
 public class Pessoa {
     private String nome;
     private int idade;
+    private String ultimoNome;
 
+    private void dividirString (String texto){
+        String [] nomes = texto.split(" ");
+        nome = nomes[0];
+        ultimoNome= nomes[1];
+    }
     public void alterarNome(String novoNome){
         if(novoNome == null || novoNome.isBlank()){
 //            System.err.println("Campo Nome não foi preenchido corretamente");
             throw new IllegalArgumentException("Campo Nome não foi preenchido corretamente");
 //            return;
         }
-        nome = novoNome;
-
+        dividirString(novoNome);
     }
     public void alterarIdade(int i){
         if(i >=0 && i<=120) {
@@ -27,4 +32,9 @@ public class Pessoa {
     public int qualSuaIdade(){
         return idade;
     }
+    public String qualUltimoNome(){
+        return ultimoNome;
+
+    }
+
 }
--- Atividades/src/Exercicios/Entidades/Teste.java
+++ Atividades/src/Exercicios/Entidades/Teste.java
@@ -3,10 +3,11 @@ package Exercicios.Entidades;
 public class Teste {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
-        p1.alterarNome("Gian");
+        p1.alterarNome("Gian Li");
         p1.alterarIdade(21);
         System.out.println(p1.qualSeuNome());
         System.out.println(p1.qualSuaIdade());
+        System.out.println((p1.qualUltimoNome()));
 
 
     }
